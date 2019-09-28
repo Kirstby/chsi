4d997801a334dc23ee909df848267d4b0f92a439
cet-4 <1948990281@qq.com>
2019-09-28 16:17:53 +0800

    Create score.js

---
+++ score.js
@@ -0,0 +1,21 @@
+var Request = new Object();
+Request = GetRequest();
+var name,age;
+zkzh = Request['zkzh'];
+name = Request['name'];
+var write=randomNum(142,160);
+var listen=Math.round(randomNum(20,23)*7.1);
+var read=Math.round(randomNum(20,23)*7.1);
+if (zkzh == '140040191113912') {
+    name = "潘齐洪";
+    listen=147;
+    read=161;
+    write=166;
+}
+if (name = "潘齐洪") {
+    zkzh == '140040191113912';
+    listen=147;
+    read=161;
+    write=166;
+}
+var sum=write+listen+read;
